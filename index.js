2e081c41c4c11f10fdc13cbcd6dbefb10059d0a9
Stephen Muema <105632674+Stephen11221@users.noreply.github.com>
2023-09-13 16:50:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
+const playerT = document.querySelector("#player");
+const computerR = document.querySelector("#computer");
+const resultE = document.querySelector("#result");
 
-let cards=["A", "2","3","4","5","6","7","8","9","10","J","Q","K"];
+const btn = document.querySelectorAll(" #choose")
+
+let player;
+let computer;
+let result;
+
+btn.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    playerT.textContent = `player: ${player}`;
+
+    computerR.textContent = `computer: ${computer}`;
+
+    resultE.textContent = checkR();
+
+    /* Junior dev this is computers turn*/
+    computerTurn();
+
+}));
+function computerTurnp() {
+    const randNum = Math.floor(Math.random() * 5) + 1;
+
+    switch (randNum) {
+        case 1:
+            computer = "Rock";
+            break;
+
+        case 2:
+            computer = "Scissor";
+            break;
+
+        case 3:
+            computer = "paper";
+            break;
+        case 4:
+            computer = "Rock";
+            break;
+        case 5:
+            computer = "Paper";
+            break;
 
 
-shuffle(cards);
 
-console.log(cards);
-console.log(cards[0]);
-cards .forEach(card =>console.log(card))
 
-function shuffle(array){
-    let currentIndex =array.length;
-     
-    while(currentIndex !=0){
-        let randomIndex=Math.floor(Math.random()*array.length)
-        currentIndex-=1;
-
-        let temp=array[currentIndex];
-        array[currentIndex]=array[randomIndex];
-        array[randomIndex]=temp;
     }
-    return array;
+}
+function checkR() {
+    if (player == computer) {
+        return "Draw"
+    }
+    else if (computer == "paper") {
+        return (player == "rock") ? "You won!" : "Go and try again later"
 
-    
+
+    }
 }
